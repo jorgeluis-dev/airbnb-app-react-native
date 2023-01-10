a94ddd2cf4d35320d109ec59212181c5eeae67b8
Jorge Luis Sampaio de Oliveira <jorgeluis.geek@gmail.com>
2023-01-09 22:46:35 -0300

    AirBNB-mobile-React Native

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/routes'
+
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world</Text>
+      <Text>Jorge</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#028090',
+    fontSize: 50,
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Routes from './src/routes'
+
+const Routes = () => <Routes />
+
+export default Routes;
